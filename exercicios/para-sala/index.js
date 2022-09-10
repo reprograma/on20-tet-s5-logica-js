e3b016a8d3296bad2e3f46f886379fb3a86cafb7
Patricia Lima <pati.lima0122@gmail.com>
2022-09-10 17:26:09 -0300

    resolução do exercício

---
+++ exercicios/para-sala/index.js
@@ -0,0 +1,20 @@
+
+let email = prompt("Digite o email") 
+
+let senha = prompt("Digite a senha:")
+
+
+
+if (email === "teste@reprograma.com" && senha === "teste123"){
+    alert("acesso permitido");
+}else{
+    alert ("email invalido")
+}
+
+
+
+
+
+
+
+
