8711aae89011e849749d2f34074d2962625be93b
Patricia Lima <pati.lima0122@gmail.com>
2022-09-15 01:22:36 -0300

    Resolução das atividades em JS

---
+++ exercicios/para-casa/index.js
@@ -0,0 +1,26 @@
+let email = prompt("Digite seu email:");
+console.log(email);
+
+let senha = prompt("Digite sua senha:");
+console.log(senha);
+
+
+
+
+//caso um deles esteja errado escrever uma mensagem de erro e perguntar e-mail e senha novamente
+if (email== "teste@reprograma.com" && senha == "teste123"){ 
+    alert("Acesso permitido");
+    }
+    else {
+        // alert("Email ou Senha inválido");
+        
+        while (email != "teste@reprograma.com" || senha != "teste123") {
+            alert("Email ou senha inválidos");
+            email = prompt("Digite novamente seu login")
+            senha= prompt("Digite senha novamente")
+            
+            
+            
+        }
+        alert ('Acesso liberado!')
+      }
