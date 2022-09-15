eb1a70e0c5ea5327bb1261f125e657bc61096cf0
Ana Beatriz Morita <anabemorita@gmail.com>
2022-09-14 23:04:22 -0300

    Adicionar exercício para revisão

---
+++ exercicios/revisao/index.js
@@ -0,0 +1,15 @@
+let numeroSorteado = Math.floor(Math.random()*(50 - 0 + 1));
+
+let palpite = Number(prompt("Adivinhe o número"))
+
+while (numeroSorteado !== palpite) {
+  if (isNaN(palpite)) {
+    alert("Por favor digite apenas números")
+  } else if (numeroSorteado > palpite) {
+    alert("Você errou, o número sorteado é maior")
+  } else {
+    alert("Você errou, o número sorteado é menor")
+  }
+  palpite = Number(prompt("Adivinhe o número"))
+}
+alert("Parabéns, você acertou o número")
