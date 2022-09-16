a81c505725ad3231e96ce1addd4756d55500bc0b
Anne Caroline Fonseca <annecarolineecosta@gmail.com>
2022-09-16 18:34:07 -0300

    exerxcicio de teste de login com JS

--- exercicios/para-sala/login.js
+++ exercicios/para-sala/login.js
@@ -9,8 +9,8 @@ if (email != "teste@reprograma.com" && senha=="teste123") {
 else {
     while (email !="teste@reprograma.com" || senha !="teste123") {
         alert("E-mail ou senha incorretos");
-        email = prompt("Digite seu e-mail mais uma vez")
-        senha = prompt("Digite a sua senha mais uma vez")
+        email = prompt("Digite seu e-mail mais uma vez.")
+        senha = prompt("Digite a sua senha mais uma vez.")
     }
-    alert("Seja bem vindo. Seu acesso foi permitido!")
+    alert("Seja bem vindo. Seu acesso foi permitido!!")
 }
